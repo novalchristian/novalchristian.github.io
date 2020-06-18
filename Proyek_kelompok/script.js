e20072880ffe29a5e777709a4b7e5e782be000f8
novalchristian <48008596+novalchristian@users.noreply.github.com>
2020-06-18 10:13:49 +0700

    Add files via upload

---
+++ Proyek_kelompok/script.js
@@ -0,0 +1,13 @@
+// $('.page-scroll').on('click', function(e)){
+    
+//     // Ambil isi href
+//     var tujuan = $(this).attr('href');
+
+//     // tangkap elemen yang bersangkutan
+//     var elemenTujuan = $(tujuan);
+
+//     // Pindahkan Scroll
+//     $('body').animate({
+//         scrollTop: elemenTujuan.offset().top - 50
+//     }, 1000);
+// });
